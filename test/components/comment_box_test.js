44a600bc1153ebff2ce8abba60aac6e28ab3cc1a
micahcourey <micahjcourey@gmail.com>
2016-04-27 19:23:47 -0700

    Refactor comment box test using the beforeEach Mocha method to make to test code more dry

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -2,12 +2,20 @@ import { renderComponent , expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox);
+  });
+
+  it('has the class comment-box', () => {
+    expect(component).to.have.class('comment-box');
+  });
   it('has a text area', () => {
-    const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
   });
 
   it('has a button', () => {
-
+    expect(component.find('button')).to.exist;
   });
 });

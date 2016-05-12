2965618fa189be278aba882f7ca4ba577efb1ddd
micahcourey <micahjcourey@gmail.com>
2016-05-11 19:11:40 -0700

    Add styling

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -23,10 +23,11 @@ export default class CommentBox extends Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+        <h4>Add a comment</h4>
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)}
-        />
+        /><br/>
         <button action="submit">Submit</button>
       </form>
     );
